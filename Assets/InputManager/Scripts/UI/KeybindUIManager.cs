60dd5c2237b0c15fc6c296b1826d1e3d708be789
Rink Wilbrink <rinkwilbrink1@gmail.com>
2020-09-14 11:42:43 +0200

    Added a link to the Google doc to manage time, and started working on UI code for remapping keys

---
+++ Assets/InputManager/Scripts/UI/KeybindUIManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeybindUIManager : MonoBehaviour
+{
+    // Variables
+    [Header("All GameObjects related to the Keybind / Rebind menu")]
+    [SerializeField] private GameObject ContentObject;
+
+    // Private Variables
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
